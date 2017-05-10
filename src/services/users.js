4568653f97d3814f78fb45dea586c9bf0fd3e6ca
bezoslee <lidongjies@gmail.com>
2017-05-10 10:59:19 +0800

    sync code

--- src/services/users.js
+++ src/services/users.js
@@ -2,6 +2,14 @@ import { request, config } from '../utils'
 const { api } = config
 const { users } = api
 
+// export async function query (params) {
+//   return request({
+//     url: 'http://localhost:4000/api/v1/users',
+//     method: 'get',
+//     data: params,
+//   })
+// }
+//
 export async function query (params) {
   return request({
     url: users,
--- src/utils/config.js
+++ src/utils/config.js
@@ -7,7 +7,7 @@ module.exports = {
   iconFontJS: '/iconfont.js',
   baseURL: 'http://localhost:8000/api/v1',
   YQL: ['http://www.zuimeitianqi.com'],
-  CORS: ['http://localhost:7000'],
+  CORS: ['http://localhost:7000', 'http://192.168.1.11:8080', 'http://localhost:4000'],
   openPages: ['/login'],
   apiPrefix: '/api/v1',
   api: {
